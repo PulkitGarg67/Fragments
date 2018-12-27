fee4637358f03f3d768767edfa4d9f99dfa4bec4
Pulkit Garg <pulkitgarg6776@gmail.com>
2018-12-27 12:38:28 +0530

    first commit

---
+++ app/src/main/java/cic/du/ac/in/all_about_fragments/MainActivity.java
@@ -0,0 +1,13 @@
+package cic.du.ac.in.all_about_fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
