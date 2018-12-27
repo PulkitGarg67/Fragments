cfdacb2516081802d880650eb844137392f745b0
Pulkit Garg <pulkitgarg6776@gmail.com>
2018-12-27 13:37:08 +0530

    Add a static fragment

---
+++ app/src/main/java/cic/du/ac/in/all_about_fragments/StaticFragment.java
@@ -0,0 +1,22 @@
+package cic.du.ac.in.all_about_fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Scorpion on 12/27/2018.
+ */
+
+public class StaticFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_static,container,false);
+    }
+}
